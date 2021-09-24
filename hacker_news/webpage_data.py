22b2c9348e4592fc63300d33b3eb208e68bc853e
Esteban Fernandez Parada <esteban.py.dev@gmail.com>
2021-09-24 13:40:46 +0200

    Intial setup for webscraper project

---
+++ hacker_news/webpage_data.py
@@ -0,0 +1,67 @@
+from requests import Session as rqs
+from requests import ConnectTimeout, ConnectionError, HTTPError
+
+
+class WebPageData:
+    """
+    Class named 'WebPageData', represents the webpage data from hacker news
+
+    Attributes
+    ----------
+    url_hn : str
+        The source 'url_hn' for getting data from hacker news webpage.
+    """
+    url_hn = "https://news.ycombinator.com/news"
+
+    @classmethod
+    def webpage_txt(cls):
+        """
+        Class method called "webpage_txt", fetch data from webpage by "GET"
+        method.
+
+        Returns
+        -------
+        r.text : str
+            The 'r.text' is the string representation of the website.
+
+        Raises
+        ------
+        ConnectionError
+            If maximum numbers of calls is exceed or it there is genuine
+            connection error to webpage.
+
+        ConnectionTimeout
+            If a server does not response within 3s of a call.
+
+        HTTPError
+            If a http error is prompted.
+
+        See Also
+        --------
+        requests.session.Session:
+            For more information about session instance object.
+
+        requests.api.get:
+            For more information about parameter 'timeout' and GET-method
+            used.
+
+        """
+        connector = rqs()
+        count = 0
+        while count <= 5:
+            count += 1
+            try:
+                if count > 5:
+                    raise ConnectionError("Attempts exceeded threshold of "
+                                          "maximum number of calls to "
+                                          "webpage.")
+                r = connector.get(url=cls.url_hn, timeout=3)
+
+            except (ConnectionError, ConnectTimeout) as errors:
+                print(f"{r.reason}:\n {errors}")
+                continue
+            except HTTPError as error:
+                print(f"{r.reason}:\nHTTP error: {error}")
+                continue
+            else:
+                return r.text
